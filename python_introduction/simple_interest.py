bfcb97104a4e06521ffd38b57210def05efe54af
fred <86912882+fredDecodes@users.noreply.github.com>
2024-12-19 12:12:03 +0000

    Add py script to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+# formula for simple interest
+'''
+simple_interest = P * R * T,
+where P = Principal(actual amount)
+      R = Rate (in percentage terms)
+      T = Time (hours, days, months, years)
+'''
+principal = 1000; 
+rate = 0.05; 
+time = 3; 
+
+interest = principal * rate * time; 
+
+print("The simple interest is: " + str(interest)); 
