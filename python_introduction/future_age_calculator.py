e334a29d115316222d12f3decc69bd2f22d3482b
fred <86912882+fredDecodes@users.noreply.github.com>
2024-12-19 13:45:05 +0000

    Update age calculator script

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -7,7 +7,7 @@ current_year = 2023;
 future_year = 2050; 
 year_interval = future_year - current_year; 
 
-current_age = input("How old are your\?\s"); 
+current_age = input("How old are your? \n"); 
 
 age = int(current_age) + year_interval; 
 print("In " + str(future_year) + ", you will be " + str(age) + " years old."); 
