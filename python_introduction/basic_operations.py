eb363a409e911de39e3d8413cab0a31b2213c467
fred <86912882+fredDecodes@users.noreply.github.com>
2024-12-19 11:57:02 +0000

    Add py sscript to perform basic math operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10;
+number2 = 5;
+
+add = number1 + number2;
+difference = number1 - number2;
+product = number1 * number2;
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(add))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(product))
