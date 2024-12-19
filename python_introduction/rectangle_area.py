e29247e4cbc14e5ca56fa490c034df31dbd248ce
fred <86912882+fredDecodes@users.noreply.github.com>
2024-12-19 12:19:47 +0000

    Add py script to calculate area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# formula for area of a rectangle
+'''
+area_of_rectangle = L * W,
+where L = length,
+      W = width
+'''
+length = 10; 
+width = 5; 
+
+area = length * width; 
+
+print("The area of the rectangle is: " + str(area)); 
