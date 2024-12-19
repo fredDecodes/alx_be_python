08d3ddc1cc87ea92d65e3350878e67d2f003fbc2
fred <86912882+fredDecodes@users.noreply.github.com>
2024-12-19 14:40:55 +0000

    Update finance_calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 # script to calculate potential future savings
-monthly_income = int(input("Enter your monthly salary: ")); 
-monthly_expenses = int(input("Enter your total monthly expenses: ")); 
+monthly_income = float(input("Enter your monthly salary: ")); 
+monthly_expenses = float(input("Enter your total monthly expenses: ")); 
 interest_rate = 0.05; 
 months_in_year = 12; 
 
@@ -10,5 +10,5 @@ annual_savings = monthly_savings * months_in_year;
 interest = monthly_savings * months_in_year * interest_rate; 
 projected_savings = annual_savings + interest; 
 
-print("Your monthly savings are: GHC" + str(monthly_savings)); 
-print("Projected savings after one year, with interest is: GHC" + str(projected_savings)); 
+print(f"Your monthly savings are: GHC${monthly_savings}"); 
+print(f"Projected savings after one year, with interest is: GHC${projected_savings}"); 
