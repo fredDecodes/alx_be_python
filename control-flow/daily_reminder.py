ed5cc896bf6096c66c028fde8f143cd9124ca3a7
fred <86912882+fredDecodes@users.noreply.github.com>
2025-09-26 00:02:54 +0100

    Update user prompts

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,7 +1,7 @@
 # Personal Daily Reminder Application
 task = input("Enter your task: ")
-priority = input("Enter the task priority (high, medium, low): ").lower()
-time_bound = input("Is this task time-bound? (yes/no): ").lower()
+priority = input("Priority (high, medium, low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match (priority):
     case "high":
