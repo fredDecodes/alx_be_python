ce305c4b5ecb825fd84620b61f6b378b7f348766
fred <86912882+fredDecodes@users.noreply.github.com>
2025-10-02 12:35:00 +0100

    Update match case condition, if expressions

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,17 +5,17 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        if (time_bound == "yes"):
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task}' is a high priority task. Try to complete it as soon as possible!")
     case "medium":
-        if (time_bound == "yes"):
+        if time_bound == "yes":
             print(f"Reminder: {task} requires moderate attention, should be completed the next 3 days!")
         else:
             print(f"Reminder: {task} is a medium priority task. Try to complete it when you can.")
     case "low":
-        if (time_bound == "yes"):
+        if time_bound == "yes":
             print(f"Reminder: {task} is a low priority task but has a deadline. Try to complete it within the week.")
         else:
             print(f"Reminder: {task} is a low priority task. Consider completing it when you have free time.")
